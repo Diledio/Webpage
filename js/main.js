dd673dfd222faf6c42cfae329ff3de707154145e
Diledio <diledio12@gmail.com>
2021-07-15 02:21:24 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContetLoaded", (event) => {
+    let topButton = document.getElementById("topButton")
+
+    window.onscroll = () => {
+
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            topButton.style.display = "block";
+        } else {
+            topButton.style.display = "none"
+        }
+
+    }
+
+
+});
